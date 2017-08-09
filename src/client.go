ada4b06566ea40b4f7943b64a15a5af06c31d01d
Hauke Stieler <mail@hauke-stieler.de>
2017-08-10 00:10:54 +0200

    Implement register method

--- src/client.go
+++ src/client.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
+	"os"
+	"time"
 )
 
 func main() {
 	client, err := Connect("localhost", "55545")
-	if err == nil {
-		client.Close()
-	} else {
+	if err != nil {
 		fmt.Println(err.Error())
+		os.Exit(1)
 	}
+	time.Sleep(time.Second)
+
+	err = client.Register("golang", "news")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	time.Sleep(time.Second)
+
+	client.Close()
 }
 
 type GomsClient struct {
-	connection net.Conn
+	connection *net.Conn
 }
 
 func Connect(address string, port string) (*GomsClient, error) {
@@ -25,18 +37,28 @@ func Connect(address string, port string) (*GomsClient, error) {
 	}
 
 	result := GomsClient{
-		connection: connection,
+		connection: &connection,
 	}
 
 	return &result, nil
 }
 
-func (client *GomsClient) Register(topics ...string) {
+func (client *GomsClient) Register(topics ...string) error {
+	message := getRegisterMessage(topics)
+	data, err := json.Marshal(message)
 
+	if err != nil {
+		return err
+	}
+
+	data = append(data, '\n')
+	_, err = (*client.connection).Write(data)
+
+	return err
 }
 
 func (client *GomsClient) Close() {
 	//TOOD send logout message
 
-	client.connection.Close()
+	(*client.connection).Close()
 }
